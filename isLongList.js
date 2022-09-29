ecddb55861514c2bb54cbca7400ceac99699a2ea
JakeRoughton <112968037+JakeRoughton@users.noreply.github.com>
2022-09-29 11:09:31 -0400

    Update isLongList.js

--- isLongList.js
+++ isLongList.js
@@ -1,8 +1,9 @@
 const prompt = require('prompt-sync')({sigint: true});
 
 let userArray = JSON.parse(prompt("Enter an array: "))
+
 let myBool = userArray.length
-    if (myBool <=10){
-    console.log("You do not have the minimum amount of characters")
-}  else if (myBool >=11)
-    console.log("You have the minimum amount of characters!!")
+if (myBool <10){
+console.log("You do not have the minimum amount of characters")
+} else if (myBool >=10)
+console.log("You have the minimum amount of characters!!")
